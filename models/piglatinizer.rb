27800f5b749e0f7c9baf811419b6b10998ef74fb
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-04-17 14:41:00 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,7 +3,7 @@ class PigLatinizer
   end
 
   def piglatinize(text)
-    text.split(" ").collect do |word| 
+    text.split(" ").collect do |word|
       parts = word.partition(/[aeiou]/i)
       parts[0] == "" ? parts[1] + parts[2] + 'way' : parts[1] + parts[2] + parts[0] + 'ay'
     end.join(" ")
